95ab66ed894ce53c993a07960dfb31b6aed66082
Rzay_ri48@itstep.edu.az <Rzay_ri48@itstep.edu.az>
2022-07-20 12:39:36 +0400

    Add project files.

---
+++ Tcp_server_TAPSIRIQ/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+
+namespace Tcp_server_TAPSIRIQ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public class pro
+        {
+
+            public string process_name { get; set; }
+            public int id { get; set; }
+        }
+      
+            static TcpListener listener = null;
+            static
+            public BinaryReader br
+            { get; set; }
+            public ObservableCollection<string> processes { get; set; }
+
+            private void StartServer()
+            {
+                var ip = IPAddress.Parse("10.2.27.46");
+                var ep = new IPEndPoint(ip, 61932);
+                listener = new TcpListener(ep);
+                listener.Start();
+                var client = listener.AcceptTcpClient();
+
+
+
+                Task.Run(() =>
+                {
+
+                    
+
+                    try
+                    {
+
+                        var json_name = "";
+                        var stream = client.GetStream();
+                        br = new BinaryReader(stream);
+                        json_name = br.ReadString();
+                        var lazimli = JsonConvert.DeserializeObject<ObservableCollection<string>>(json_name);
+                        foreach (var pr in lazimli)
+                        {
+                            dispatcher.Invoke(() => processes.Add(pr.ToString()));
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                });
+            }
+            private Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
+
+            public MainWindow()
+            {
+
+                InitializeComponent();
+                processes = new ObservableCollection<string>();
+                Thread newThread = new Thread(new ThreadStart(StartServer));
+                newThread.Start();
+                DataContext = this;
+            }
+        }
+    
+}
---
+++ Tcp_tapsiriq_client/MainViewModel.cs
@@ -0,0 +1,214 @@
+﻿
+using GalaSoft.MvvmLight.Command;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+
+namespace Tcp_tapsiriq_client
+{
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private List<string> list_combo;
+
+        public List<string> List_Combo
+        {
+            get { return list_combo; }
+            set { list_combo = value; OnPropertyChanged(); }
+        }
+
+        private string textbox_text;
+
+        public string Textbox_text
+        {
+            get { return textbox_text; }
+            set { textbox_text = value; OnPropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public ObservableCollection<string> processes { get; set; }
+
+        private string selected_combobox;
+
+        public string Selected_combobox
+        {
+            get { return selected_combobox; }
+            set { selected_combobox = value; OnPropertyChanged(); }
+        }
+
+        public RelayCommand Run
+        {
+            get => new RelayCommand(() =>
+            {
+                if (Selected_combobox == "List")
+                {
+                    var client = new TcpClient();
+                    var ip = IPAddress.Parse("10.2.27.46");
+                    var ep = new IPEndPoint(ip, 61932);
+
+                    try
+                    {
+                        client.Connect(ep);
+                        if (client.Connected)
+                        {
+                            while (true)
+                            {
+
+                                var stream = client.GetStream();
+                                //var bw = new BinaryWriter(stream);
+
+
+
+                                //foreach (var process in processes)
+                                //{
+                                //    bw.Write(process.Id);
+                                //    bw.Write(process.ProcessName.ToString());
+                                //}
+                                var bw = new BinaryWriter(stream);
+                                var json = JsonConvert.SerializeObject(processes, Newtonsoft.Json.Formatting.Indented);
+                                bw.Write(json);
+
+
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (Selected_combobox == "kill")
+                {
+                    var client = new TcpClient();
+                    var ip = IPAddress.Parse("10.2.27.46");
+                    var ep = new IPEndPoint(ip, 61932);
+
+                    try
+                    {
+                        client.Connect(ep);
+                        if (client.Connected)
+                        {
+                            while (true)
+                            {
+
+                                var stream = client.GetStream();
+                                try
+                                {
+
+                                    var lst = Process.GetProcessesByName(Textbox_text);
+                                    foreach (var proc in lst)
+                                    {
+                                        proc.Kill();
+                                    }
+                                    processes.Clear();
+                                    foreach (var process in Process.GetProcesses())
+                                    {
+                                        processes.Add(process.ProcessName);
+
+                                    }
+                                }
+                                catch (Exception)
+                                {
+
+                                    throw;
+                                }
+                               
+                                var bw = new BinaryWriter(stream);
+                                var json = JsonConvert.SerializeObject(processes, Newtonsoft.Json.Formatting.Indented);
+                                bw.Write(json);
+
+
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else if (Selected_combobox == "Start")
+                {
+                    var client = new TcpClient();
+                    var ip = IPAddress.Parse("10.2.27.46");
+                    var ep = new IPEndPoint(ip, 61932);
+
+                    try
+                    {
+                        client.Connect(ep);
+                        if (client.Connected)
+                        {
+                            while (true)
+                            {
+
+                                var stream = client.GetStream();
+                                try
+                                {
+
+                                    var lst = Process.GetProcessesByName(Textbox_text);
+                                    foreach (var proc in lst)
+                                    {
+                                        proc.Start();
+                                    }
+                                    processes.Clear();
+                                    foreach (var process in Process.GetProcesses())
+                                    {
+                                        processes.Add(process.ProcessName);
+
+                                    }
+                                }
+                                catch (Exception)
+                                {
+
+                                    throw;
+                                }
+
+                                var bw = new BinaryWriter(stream);
+                                var json = JsonConvert.SerializeObject(processes, Newtonsoft.Json.Formatting.Indented);
+                                bw.Write(json);
+
+
+                            }
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            });
+        }
+        public MainViewModel()
+        {
+
+            processes = new ObservableCollection<string>();
+            foreach (var process in Process.GetProcesses())
+            {
+                processes.Add(process.ProcessName);
+
+            }
+            List_Combo = new List<string>();
+            List_Combo.Add("List");
+            List_Combo.Add("Kill");
+            List_Combo.Add("Create");
+            List_Combo.Add("help");
+
+
+        }
+    }
+}
